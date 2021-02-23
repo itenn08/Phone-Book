f438be1bead285a4c99449095044aaac67a7b389
Kostya Rybka <itenn08@gmail.com>
2021-02-23 18:53:11 +0300

    added throttle and callback for search

--- src/components/ClientList/SearchForm.jsx
+++ src/components/ClientList/SearchForm.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Input } from "semantic-ui-react";
 import { useDispatch } from "react-redux";
 import { searchClient } from "../../redux/actions/client.actions";
+import throttle from "lodash.throttle";
 
 const SearchForm = () => {
   const dispatch = useDispatch();
+
+  const search = useCallback(
+    throttle((value) => {
+      dispatch(searchClient(value));
+      console.log(value);
+    }, 500),
+    []
+  );
+
   return (
     <Input
       icon={{ name: "search", circular: true, link: true }}
       placeholder="Search..."
-      onChange={(e) => dispatch(searchClient(e.target.value))}
+      onChange={(e) => search(e.target.value)}
       fluid
     />
   );
